bbe8370a8c6d1f4867e4b029c88ff2d68fee4bb2
smyaglikovs-web <smyaglikovs@gmail.com>
2026-08-01 19:18:34 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@utils': path.resolve(__dirname, './src/frontend/utils'),
+    },
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
